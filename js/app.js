754150242d41fbe22d52bcf7434f9cacbcaa621b
tinselcity <reed.morrison@gmail.com>
2023-02-12 21:39:42 -0800

    adding tooltips to map.

--- js/app.js
+++ js/app.js
@@ -5,31 +5,260 @@ var g_host = "http://127.0.0.1:12346";
 //! ----------------------------------------------------------------------------
 //! world map
 //! ----------------------------------------------------------------------------
-var width = $("#map #amazingViz").width(); //scope.width || 300;
-var height = $("#map #amazingViz").height(); //scope.height || 1020;
-var scale = 1; //scope.scale || 1;
-var maxRadius = 20; //scope.maxRadius || 20;
-var projection = d3.geoMercator().
-    center([0, 25]).
-    scale((width + 1) / 2 / Math.PI).
-    translate([width / 2, height / 2]).
-    precision(.1);
+var g_width = $("#map #amazingViz").width();
+var g_height = $("#map #amazingViz").height();
+var g_projection = d3.geoMercator()
+    .center([0, 25])
+    .scale((g_width + 1) / 2 / Math.PI)
+    .translate([g_width / 2, g_height / 2])
+    .precision(.1);
 var g_map_svg = d3.select("#map #amazingViz").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", g_width)
+    .attr("height", g_height);
 var g_g = g_map_svg.append("g");
-var path = d3.geoPath().projection(projection);
+var g_path = d3.geoPath().projection(g_projection);
+// ---------------------------------------------------------
 // load and display the World
+// ---------------------------------------------------------
 d3.json("data/topojson/world-110m2.json").then(function(topology) {
     g_g.selectAll("path")
         .data(topojson.feature(topology, topology.objects.countries).features)
         .attr("class", "feature")
         .enter().append("path")
-        .attr("d", path)
+        .attr("d", g_path)
         .style("fill", "#EEEEEE")
         .style("stroke", "#BDBDBD")
         .style("stroke-width", "0.5");
 });
+//! ----------------------------------------------------------------------------
+//! ****************************************************************************
+//!                      Z O O M   A N D   P A N
+//! ****************************************************************************
+//! ----------------------------------------------------------------------------
+/*
+var g_translate = [0,0];
+var g_scale = 1;
+var zoom = d3.zoom()
+    .on("zoom",function() {
+        g_translate = d3.event.translate;
+        g_scale = d3.event.scale;
+        g_g.attr("transform","translate("+ g_translate.join(",")+")scale("+g_scale+")");
+        d3.selectAll("#bonkers").attr("transform","translate("+ g_translate.join(",")+")scale("+g_scale+")");
+});
+*/
+//! ----------------------------------------------------------------------------
+//! TODO
+//! ----------------------------------------------------------------------------
+/*
+g_map_svg.call(zoom);
+*/
+//! ----------------------------------------------------------------------------
+//! TODO
+//! ----------------------------------------------------------------------------
+/*
+function zoomClick() {
+    var clicked = d3.event.target,
+        direction = 1,
+        factor = 0.4,
+        target_zoom = 1,
+        center = [width / 2, height / 2],
+        extent = zoom.scaleExtent(),
+        translate = zoom.translate(),
+        translate0 = [],
+        l = [],
+        view = {x: translate[0], y: translate[1], k: zoom.scale()};
+    d3.event.preventDefault();
+    direction = (this.id === 'zoom_in') ? 1 : -1;
+    target_zoom = zoom.scale() * (1 + factor * direction);
+    if (target_zoom < extent[0] || target_zoom > extent[1]) { return false; }
+    translate0 = [(center[0] - view.x) / view.k, (center[1] - view.y) / view.k];
+    view.k = target_zoom;
+    l = [translate0[0] * view.k + view.x, translate0[1] * view.k + view.y];
+    view.x += center[0] - l[0];
+    view.y += center[1] - l[1];
+    interpolateZoom([view.x, view.y], view.k);
+}
+*/
+//! ----------------------------------------------------------------------------
+//! TODO
+//! ----------------------------------------------------------------------------
+/*
+function zoomed() {
+    g_g.attr("transform",
+        "translate(" + zoom.translate() + ")" +
+        "scale(" + zoom.scale() + ")"
+    );
+    d3.selectAll("#bonkers").attr("transform","translate("+
+        zoom.translate().join(",")+")scale("+zoom.scale()+")");
+}
+*/
+//! ----------------------------------------------------------------------------
+//! TODO
+//! ----------------------------------------------------------------------------
+/*
+function interpolateZoom (translate, scale) {   
+    var self = this;
+    return d3.transition().duration(350).tween("zoom", function () {
+        var iTranslate = d3.interpolate(zoom.translate(), translate),
+            iScale = d3.interpolate(zoom.scale(), scale);
+        return function (t) {
+            zoom
+                .scale(iScale(t))
+                .translate(iTranslate(t));
+            zoomed();
+        };
+    });
+}
+// don't zoom on scroll
+d3.selectAll('.zoom-btn').on('click', zoomClick);
+g_map_svg.on("wheel.zoom", null);
+*/
+//! ----------------------------------------------------------------------------
+//! show_peers
+//! ----------------------------------------------------------------------------
+var show_map = function(a_data)
+{
+    // -----------------------------------------------------
+    // vars
+    // -----------------------------------------------------
+    var l_data_active = [];
+    var l_data_none = [];
+    var l_max = 1000;
+    var l_count_active = 0;
+    var l_count_none = 0;
+    // -----------------------------------------------------
+    // peers
+    // -----------------------------------------------------
+    var l_peers = a_data["peers"];
+    for (var i = 0; i < l_peers.length; i++) {
+        var i_obj = l_peers[i];
+        if ((i_obj["status"] == "CONNECTED") &&
+            (l_count_active < l_max))  {
+            l_data_active.push(i_obj)
+            ++l_count_active;
+        }
+        else if ((i_obj["status"] != "CONNECTED") &&
+                 (l_count_none < l_max)){
+            l_data_none.push(i_obj)
+            ++l_count_none;
+        }
+    }
+    // -----------------------------------------------------
+    // clean last
+    // -----------------------------------------------------
+    d3.selectAll("#peers_map").remove();
+    var g_peers = g_map_svg.append("g").attr("id","peers_map");
+    // -----------------------------------------------------
+    //
+    // -----------------------------------------------------
+    // create a tooltip
+    var l_tooltip = d3.select("#map #amazingViz")
+      .append("div")
+      .attr("class", "tooltip fw-bold mt-5")
+      .style("visibility", "hidden")
+      .style("background-color", "silver")
+      .style("opacity", 0.8)
+      .style("border", "solid")
+      .style("border-width", "1px")
+      .style("border-radius", "5px")
+      .style("padding", "5px");
+
+    var mouseover_inactive = function(event, d) {
+        l_tooltip
+            .style("visibility", "visible")
+            .style("background-color", "#92A8D1")
+            .style("top", (event.pageY)+"px")
+            .style("left",(event.pageX)+"px")
+            .html("<p>"+d.host+"</p>"+"<p>"+d.geoip2_city+", "+d.geoip2_country+"</p>");
+        d3.select(this)
+            .attr("r", 10)
+            .style("fill", "#92A8D1")
+            .style("fill-opacity", "0.7");
+    }
+    var mouseover_active = function(event, d) {
+        l_tooltip
+            .style("visibility", "visible")
+            .style("background-color", "#F7CAC9")
+            .style("top", (event.pageY)+"px")
+            .style("left",(event.pageX)+"px")
+            .html("<p>"+d.client+"</p>"+"<p>"+d.host+"</p>"+"<p>"+d.geoip2_city+", "+d.geoip2_country+"</p>");
+        d3.select(this)
+            .attr("r", 10)
+            .style("fill", "#F7CAC9")
+            .style("fill-opacity", "0.7");
+    }
+    var mouseleave = function(d) {
+        l_tooltip
+            .style("visibility", "hidden")
+        d3.select(this)
+            .attr("r", 5)
+            .style("fill", "gray")
+            .style("fill-opacity", "0.5");
+    }
+    // -----------------------------------------------------
+    // draw peers inactive
+    // -----------------------------------------------------
+    g_peers.selectAll("peers_none")
+        .data(l_data_none)
+        .enter()
+        .append("circle")
+        .attr("cx",function(d) {
+            return g_projection([
+                d.geoip2_lon,
+                d.geoip2_lat
+            ])[0];
+        })
+        .attr("cy",function(d) {
+            return g_projection([
+                d.geoip2_lon,
+                d.geoip2_lat
+            ])[1];
+        })
+        .style("fill", "gray")
+        .style("fill-opacity", "0.5")
+        .style("opacity", "0.4")
+        .style("stroke", "#888888")
+        .style("stroke-width", "0.5")
+        .attr("r",5)
+        .on("mouseover", mouseover_inactive)
+        .on("mouseleave", mouseleave);
+    // -----------------------------------------------------
+    // draw peers active
+    // -----------------------------------------------------
+    g_peers.selectAll("peers_active")
+        .data(l_data_active)
+        .enter()
+        .append("circle")
+        .attr("cx",function(d) {
+            return g_projection([
+                d.geoip2_lon,
+                d.geoip2_lat
+            ])[0];
+        })
+        .attr("cy",function(d) {
+            return g_projection([
+                d.geoip2_lon,
+                d.geoip2_lat
+            ])[1];
+        })
+        .style("fill", "red")
+        .style("fill-opacity", "0.7")
+        .style("opacity", "0.7")
+        .style("stroke", "#888888")
+        .style("stroke-width", "0.5")
+        .attr("r",5)
+        .on("mouseover", mouseover_active)
+        .on("mouseleave", mouseleave);
+}
+//! ----------------------------------------------------------------------------
+//! load_map
+//! ----------------------------------------------------------------------------
+var load_map = function() {
+    l_url = g_host+"/api/v0.1/peers.json";
+    $.get(l_url, function(data) {
+        show_map(data);
+    }, "json");
+}
 //! ----------------------------------------------------------------------------
 //! show_info
 //! ----------------------------------------------------------------------------
@@ -72,7 +301,7 @@ var show_trackers = function(a_data)
                 continue;
         }
         l_body.append($('<tr>').append(
-            $('<td class="col-3">').text(i_obj["host"])).append(
+            $('<td>').text(i_obj["host"])).append(
             $('<td>').text(i_obj["scrape_num_complete"])).append(
             $('<td>').text(i_obj["scrape_num_downloaded"])).append(
             $('<td>').text(i_obj["scrape_num_incomplete"])).append(
@@ -122,16 +351,21 @@ var show_peers = function(a_data)
         if (i_obj["status"] == "NONE") {
                 continue;
         }
+        // -------------------------------------------------
+        // create state string
+        // -------------------------------------------------
+        l_state = "";
         l_body.append($('<tr>').append(
-            $('<td class="col-5">').text(i_obj["host"])).append(
-            $('<td class="col-3">').text(i_obj["client"])).append(
-            $('<td class="col-2">').text(i_obj["from"])).append(
-            $('<td class="col-3">').text(i_obj["status"])).append(
-            $('<td class="col-3">').text(i_obj["recvd"])).append(
-            $('<td class="col-3">').text(i_obj["recvd_per_s"])).append(
-            $('<td class="col-3">').text(i_obj["sent"])).append(
-            $('<td class="col-3">').text(i_obj["sent_per_s"])).append(
-            $('<td class="col-2">').text(i_obj["error"])));
+            $('<td>').text(i_obj["host"])).append(
+            $('<td>').text(i_obj["client"])).append(
+            $('<td>').text(i_obj["from"])).append(
+            $('<td>').text(i_obj["status"])).append(
+            $('<td>').text(i_obj["peer_choking"])).append(
+            $('<td class="text-end">').text(i_obj["recvd"])).append(
+            $('<td class="text-end">').text(i_obj["recvd_per_s"])).append(
+            $('<td class="text-end">').text(i_obj["sent"])).append(
+            $('<td class="text-end">').text(i_obj["sent_per_s"])).append(
+            $('<td class="text-end">').text(i_obj["error"])));
     }
     l_body.trigger('update');
     // -----------------------------------------------------
@@ -156,80 +390,6 @@ var load_peers = function() {
     }, "json");
 }
 //! ----------------------------------------------------------------------------
-//! show_peers
-//! ----------------------------------------------------------------------------
-var show_map = function(a_data)
-{
-    // -----------------------------------------------------
-    // data dict
-    // -----------------------------------------------------
-    var l_data_active = {
-      "type": "FeatureCollection",
-      "features": []
-    };
-    var l_data_dead = {
-      "type": "FeatureCollection",
-      "features": []
-    };
-    // -----------------------------------------------------
-    // peers
-    // -----------------------------------------------------
-    var l_peers = a_data["peers"];
-    for (var i = 0; i < l_peers.length; i++) {
-        var i_obj = l_peers[i];
-        if (i_obj["status"] == "NONE") {
-                continue;
-        }
-        l_f = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [i_obj["geoip2_lon"], i_obj["geoip2_lat"]]
-            }
-        }
-        if (i_obj["status"] == "CONNECTED") {
-            l_data_active["features"].push(l_f);
-        }
-        else {
-            l_data_dead["features"].push(l_f);
-        }
-    }
-    // -----------------------------------------------------
-    // draw dots
-    // -----------------------------------------------------
-    d3.selectAll("#bonkers").remove();
-    var g = g_map_svg.append("g").attr("id","bonkers");
-    g.selectAll('.peers_dead')
-        .append("svg:circle")
-        .data(l_data_dead.features)
-        .enter()
-        .append('path')
-        .style("fill", "gray")
-        .style("opacity", "0.4")
-        .attr("r", 0)
-        .attr('d',path)
-        .attr('class', 'peers_dead');
-    g.selectAll('.peers_active')
-        .append("svg:circle")
-        .data(l_data_active.features)
-        .enter()
-        .append('path')
-        .style("fill", "red")
-        .style("opacity", "0.9")
-        .attr("r", 0)
-        .attr('d',path)
-        .attr('class', 'peers_active');
-}
-//! ----------------------------------------------------------------------------
-//! load_map
-//! ----------------------------------------------------------------------------
-var load_map = function() {
-    l_url = g_host+"/api/v0.1/peers.json";
-    $.get(l_url, function(data) {
-        show_map(data);
-    }, "json");
-}
-//! ----------------------------------------------------------------------------
 //! load view
 //! ----------------------------------------------------------------------------
 var load_view = function() {
@@ -245,7 +405,7 @@ var load_view = function() {
     }
     else if(l_tab_id == "map-tab") {
         load_map();
-        setTimeout(refresh_view, 1000);
+        //setTimeout(refresh_view, 1000);
     }
 }
 //! ----------------------------------------------------------------------------
